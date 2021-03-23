16f2b41174262edc665ea5090bee188681501ad9
Maria Savinova <msavinko@gmail.com>
2021-03-23 12:04:45 +0300

    Updates

---
+++ precache-manifest.4bd58b86dc600e04b279269f647e6d90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f604bbb3b7a20ba5a00aa2f3517787",
+    "url": "/natashasavinova-illustrator/index.html"
+  },
+  {
+    "revision": "0c4dc41876efc5544b0d",
+    "url": "/natashasavinova-illustrator/static/css/main.f83dbf95.chunk.css"
+  },
+  {
+    "revision": "7e130ac274bcf5f09c2c",
+    "url": "/natashasavinova-illustrator/static/js/2.8615e259.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/natashasavinova-illustrator/static/js/2.8615e259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c4dc41876efc5544b0d",
+    "url": "/natashasavinova-illustrator/static/js/main.483c215e.chunk.js"
+  },
+  {
+    "revision": "e0e537d51af4a202af4c",
+    "url": "/natashasavinova-illustrator/static/js/runtime-main.dcc8fb78.js"
+  },
+  {
+    "revision": "3d1908231c04c74b76e067621ff191ee",
+    "url": "/natashasavinova-illustrator/static/media/CeraPro-Light.3d190823.woff"
+  },
+  {
+    "revision": "2558fd3ae2d244ceab53b6b455731490",
+    "url": "/natashasavinova-illustrator/static/media/CeraPro-Regular.2558fd3a.woff"
+  }
+]);
